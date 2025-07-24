3750e7585ae5f216311c3f143ca7fc7b97de14a0
Mileñum <mileprevi@gmail.com>
2025-07-24 19:44:43 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,17 @@ export default function ChamomileTea() {
   return (
       <main className="flex bg-neutral-100 w-full h-screen justify-center items-center h-full p-4 ">
           <div className="flex flex-col mt-12 relative">
-              <Diamond />
+              <Diamond/>
               <div className="">
       <div className="flex justify-center h-screen flex-col">
-          <h1 className="text-6xl font-bold text-neutral-950 mb-4">CHAMOMILE TEA</h1>
-          <div className="text-gray-700">
-            <p className="mb-6"><span className="font-bold">Countries:</span> China - Japan - Korea - Morocco</p>
-            <p className="mb-6"><span className="font-bold">Ingredients:</span> Camellia Sinensis leaves, Steamed or pan-fired to prevent oxidation, Fresh, vegetal, slightly astringent</p>
-            <p className="mb-6"><span className="font-bold">Flavor:</span> Grassy, fresh, with subtle sweetness</p>
+          <h1 className="text-6xl font-serif text-neutral-950 mb-4 font-bold w-16">CHAMOMILE TEA</h1>
+          <div className="text-gray-700 space y-4">
+            <p className="mb-6 font-serif font-bold text-2xl text-neutral-950">Countries</p>
+              <p className="mb-6 font-serif"> China - Japan - Korea - Morocco</p>
+            <p className="mb-6 font-serif text-2xl font-bold text-neutral-950">Ingredients</p>
+               <p className="mb-6 font-serif">Camellia Sinensis leaves, Steamed or pan-fired to prevent oxidation, Fresh, vegetal, slightly astringent</p>
+            <p className="mb-6 font-serif text-2xl font-bold text-neutral-950">Flavor</p>
+              <p className="mb-6 font-serif">Grassy, fresh, with subtle sweetness</p>
           </div>
          </div>
         </div>
